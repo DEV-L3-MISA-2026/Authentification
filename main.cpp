4f40adbc143f61fc7f96bde6d97ce7dc0c18cba7
Mikajy <ialymikajy@gmail.com>
2026-07-01 10:59:48 +0300

    [Mikajy] : Testing with sample > 4s, nice score

--- main.cpp
+++ main.cpp
@@ -12,18 +12,12 @@ int main() {
     SpeechProcessor::init("../model/speech_detection.onnx");
 
     auto sp = SpeechProcessor::getInstance();
+    
     // testing the result
-    vector<float> normal_audio = sp->getEmbending("voice.wav");
-    vector<float> cleaned_audio  = sp->getEmbending("cleaned_voice.wav");
-
-    vector<float> real_audio = sp->getEmbending("../static_data/voice1.wav");
-    vector<float> cleaned_real_audio = sp->getEmbending("cleaned_voice1.wav");
-   
-    cout << cosineSimilarity(normal_audio, cleaned_audio) << endl;
-    cout << cosineSimilarity(real_audio, cleaned_audio) << endl;
-    cout << cosineSimilarity(real_audio, normal_audio) << endl;
-    cout << cosineSimilarity(cleaned_real_audio, cleaned_audio) << endl;
-    cout << cosineSimilarity(cleaned_real_audio, normal_audio) << endl;
+    vector<float> audio1 = sp->getEmbending("../static_data/anselme.wav");
+    vector<float> audio2  = sp->getEmbending("../static_data/anselme2.wav");
+    
+    std::cout << "similarity : " << cosineSimilarity(audio1, audio2) << endl;
 
     return 0;
 }
